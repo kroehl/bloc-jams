d033718cc6b196fcd4941df7ad3d9723e418e01a
Kristin <kroehl13@gmail.com>
2016-03-21 14:15:00 -0400

    Assignment 23 Interfaces Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,22 +1,34 @@
-var animatePoints = function () {
- 
-    var points = document.getElementsByClassName('point');
-    var revealPoint = function (i) {
-        // modify the point's style
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)"; 
-    };
-    
-    for(var i=0, length = points.length; i < length; i++){
-        
-        // call revealPoint and pass in the 
-        // point element at the index of `i` in the
-        // `points` collection
-        revealPoint(i);    
-    }
+var pointsArray = document.getElementsByClassName('point');
+
+var revealPoint = function (point) {
+        //modify the point's style
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1)translateY(0)";
+        point.style.msTransform = "scaleX(1)translateY(0)";
+        point.style.WebkitTransform = "scaleX(1)translateY(0)"; 
 };
+
+var animatePoints = function(points) {
+    forEach (points, revealPoint);
+};
+    
+window.onload = function(){
+    
+    if (window.innerHeight > 950){
+        animatePoints(pointsArray);
+    }
+    
+    //var sellingPoints = document.getElementsByClassName("selling-points")[0];
+    //var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event){
+        if (pointsArray[0].getBoundingClientRect().top <= 500){
+            animatePoints(pointsArray);
+        
+        }
+    });
+}
+
     
 
  
