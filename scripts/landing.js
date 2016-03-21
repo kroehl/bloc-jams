ff3fc85c8749ca97d7004c67d80af833ed3091e3
Kristin <kroehl13@gmail.com>
2016-03-20 20:15:40 -0400

    scripts/landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,7 @@
-var animatePoints = function () {
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function (points) {
  
-    var points = document.getElementsByClassName('point');
     var revealPoint = function (i) {
         // modify the point's style
         points[i].style.opacity = 1;
@@ -9,7 +10,7 @@ var animatePoints = function () {
         points[i].style.WebkitTransform = "scaleX(1) translateY(0)"; 
     };
     
-    for(var i=0, length = points.length; i < length; i++){
+    for (var i=0, length = points.length; i < length; i++){
         
         // call revealPoint and pass in the 
         // point element at the index of `i` in the
@@ -17,6 +18,22 @@ var animatePoints = function () {
         revealPoint(i);    
     }
 };
+
+window.onload = function(){
+    
+    if (window.innerHeight > 950){
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName("selling-points")[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event){
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+            animatePoints(pointsArray);
+        }
+    });
+}
     
 
  
