9165ca562bf2b78fac6da0dc63d4b8914fa42d6d
Kristin <kroehl13@gmail.com>
2016-03-22 14:26:47 -0400

    Assignment-23-interfaces-corrected

--- scripts/landing.js
+++ scripts/landing.js
@@ -17,22 +17,6 @@ window.onload = function(){
     if (window.innerHeight > 950){
         animatePoints(pointsArray);
     }
-<<<<<<< HEAD
-=======
-    
-    //var sellingPoints = document.getElementsByClassName("selling-points")[0];
-    //var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    
-    window.addEventListener('scroll', function(event){
-        if (pointsArray[0].getBoundingClientRect().top <= 500){
-            animatePoints(pointsArray);
-        
-        }
-    });
-}
-
->>>>>>> 6a63587faa1ae2ed8517c162ca3b48b20760f2ba
-    
 
     
     window.addEventListener('scroll', function(event){
@@ -42,6 +26,7 @@ window.onload = function(){
         }
     });
 }
+
 
             
            
