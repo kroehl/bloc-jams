f7a2af1bc3cd4dcca90a874fa55558acf2d5c23a
Kristin <kroehl13@gmail.com>
2016-03-31 13:42:01 -0400

    Checkpoint 26 interfaces completed

--- scripts/album.js
+++ scripts/album.js
@@ -50,7 +50,7 @@ var albumKing = {
 var createSongRow = function(songNumber, songName, songLength){
     var template = 
         '<tr class="album-view-song-item">'
-    + '  <td class="song-item-number">' + songNumber + '</td>'
+    + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
     + '  <td class="song-item-title">' + songName + '</td>'
     + '  <td class="song-item-duration">' + songLength + '</td>'
     + '</tr>'
@@ -58,6 +58,7 @@ var createSongRow = function(songNumber, songName, songLength){
  
      return template;
 };
+
 
 
     // #1
@@ -82,9 +83,28 @@ var setCurrentAlbum = function(album){
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
+
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  
 window.onload = function() {
-    setCurrentAlbum(albumPicasso); //loads first page  
+    setCurrentAlbum(albumPicasso); //loads first page 
+    
+    songListContainer.addEventListener('mouseover', function(event) {
+         
+        if(event.target.parentElement.className === 'album-view-song-item'){
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+    });
+    
+    for (var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event) {
+          this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+        });
+    }
+
     
     var albums = [albumPicasso, albumMarconi, albumKing]; 
     var index = 1;
@@ -98,6 +118,8 @@ window.onload = function() {
         }
     });
 };
+
+
 
 
 
