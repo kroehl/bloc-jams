053cf4a13b097f9feb3571b76bb86fc6a6611dc8
Kristin <kroehl13@gmail.com>
2016-05-12 13:17:21 -0400

    Checkpoint 29 interfaces: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,35 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var revealPoint = function (point) {
+var animatePoints = function() {
   
-        point.style.opacity = 1;
-        point.style.transform = "scaleX(1)translateY(0)";
-        point.style.msTransform = "scaleX(1)translateY(0)";
-        point.style.WebkitTransform = "scaleX(1)translateY(0)"; 
+    var revealPoint = function () { 
+            
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    
+    $.each($('.point'), revealPoint);
 };
 
 var animatePoints = function(points) {
     forEach (points, revealPoint);
 };
     
-window.onload = function(){
+$(window).load(function(){
     
-    if (window.innerHeight > 950){
-        animatePoints(pointsArray);
+    if ($(window).height() > 950) {
+        animatePoints();
     }
 
-    
-    window.addEventListener('scroll', function(event){
-        if (pointsArray[0].getBoundingClientRect().top <= 500){
-            animatePoints(pointsArray);
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+  
+    $(window).scroll(function(event){
+        if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
         
         }
     });
-}
+});
 
 
 
